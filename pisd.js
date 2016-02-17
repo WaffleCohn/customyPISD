c0424a2ec9410469f8f45ef1228e3e131fa66a0c
Ari Cohn <wafflecohn@gmail.com>
2016-02-17 09:44:44 -0600

    Create pisd.js Skeleton

---
+++ pisd.js
@@ -0,0 +1,29 @@
+function requestGrades()
+{
+
+}
+
+function changeName(name)
+{
+
+}
+
+function changeLogo(url)
+{
+
+}
+
+function changeProfile(url)
+{
+
+}
+
+function addGradebookTab()
+{
+
+}
+
+function addTab(html)
+{
+
+}
