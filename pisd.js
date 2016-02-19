ddd6955dc667e8059902324f4b8a7ce85d204403
WaffleCohn <Ari@Aris-MacBook-Air.local>
2016-02-19 09:21:37 -0600

    Completed Most Functions

--- pisd.js
+++ pisd.js
@@ -1,32 +1,47 @@
 function requestGrades()
 {
-
+	if (confirm("Do you want to view your grades?"))
+	{
+		goToGradebook();
+	}
 }
 
 function changeName(name)
 {
+	var studentName = document.getElementById("content").getElementsByClassName("page-title")[0].getElementsByClassName("span")[0];
 
+	studentName.innerHTML = name;
 }
 
 function changeLogo(url)
 {
+	var logo = document.getElementById("heading").getElementsByTagName("img")[0];
 
+	logo.src = url;
 }
 
 function changeProfile(url)
 {
+	var avatarPic = document.getElementById("_145_userAvatar").getElementsByTagName("img")[0];
 
-}
-
-function addGradebookTab()
-{
-
+	avatarPic.src = url;
 }
 
 function addTab(html)
 {
+	var tab = '<li class="tab">' + html + '</li>',
+		pageTabMenu = document.getElementsByClassName("pagetab-menu")[0];
 
+	pageTabMenu.innerHTML += tab;
 }
+
+function addGradebookTab()
+{
+	var gradebookTab = '<a class="pagetab" href="javascript:goToGradebook();">Gradebook</a>';
+
+	addTab(gradebookTab);
+}
+
 
 function changeTheme(color)
 {
